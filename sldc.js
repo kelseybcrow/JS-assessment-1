840d9fef4d5f6b99d38734c744a75cc20c2e188d
Kelsey Crow <kelseybcrow@gmail.com>
2021-10-29 10:29:53 -0600

    almost done

--- sldc.js
+++ sldc.js
@@ -17,9 +17,14 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
 
-let planning = "";
-let analysisOfRequirements = "";
-let design = "";
-let implementation = "";
-let testingAndIntegration = "";
-let maintain = "";
+let planning =
+  "gathering all requirements and important information, planning each step of the development process";
+let analysisOfRequirements =
+  "ironing out user expectations for a new product or feature, making sure the requirements are thorough, detailed, and quantifiable";
+let design =
+  "coming up with the structure and interaction of the code, figuring out which functions, methods, objects, etc. that will be used";
+let implementation = "the actual writing of code";
+let testingAndIntegration =
+  "testing the code that has been written during implementation, oftentimes working with a QA team, integrating the new code with the current code";
+let maintain =
+  "continually correcting bugs and providing improvements to the product or feature";
