f334c646805786732d70400d6fd0490a36c0af89
Kelsey Crow <kelseybcrow@gmail.com>
2022-01-07 10:26:04 -0700

    progress

--- password.js
+++ password.js
@@ -21,22 +21,38 @@ reader.question("What password would you like to validate?", function (input) {
   const upperCaseLetters = /[A-Z]/g;
   const numbers = /[0-9]/g;
 
-  let errorMessage = "";
+  let lengthError = "at least 10 characters";
+  let lowerError = "a lowercase letter";
+  let upperError = "an uppercase letter";
+  let numberError = "a number";
+  let errorMessage = "Invalid input, your password needs: ";
+  let successMessage = "Thank you, your password is validated";
 
   if (tokens.length >= 10) {
     correctLength = true;
   } else {
-    errorMessage = "Your password needs at least 10 characters";
+    errorMessage.concat(lengthError);
   }
 
-  if (tokens)
-    if (
-      correctLength === true &&
-      correctLower === true &&
-      correctUpper === true
-    ) {
-      console.log("Thank you, your password is validated");
-    }
+  if (tokens.value.match(lowerCaseLetters)) {
+    correctLower = true;
+  } else {
+    errorMessage.concat(lowerError);
+  }
+
+  if (tokens.value.match(upperCaseLetters)) {
+    correctUpper = true;
+  } else {
+    errorMessage.concat(upperError);
+  }
+
+  if (
+    correctLength === true &&
+    correctLower === true &&
+    correctUpper === true
+  ) {
+    console.log("Thank you, your password is validated");
+  }
 
   // This line closes the connection to the command line interface.
   reader.close();
